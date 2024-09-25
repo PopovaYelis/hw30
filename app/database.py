c392c573fd6b7b1d5cfd32a8e462e1a5909635e9
PopovaYelis <92019867+PopovaYelis@users.noreply.github.com>
2024-09-25 17:23:03 +0300

    test

--- app/database.py
+++ app/database.py
@@ -1,6 +1,5 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession 
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession , async_sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import async_sessionmaker
 
 
 DATABASE_URL = "sqlite+aiosqlite:///./app.py.db"
